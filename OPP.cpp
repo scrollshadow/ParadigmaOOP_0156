5586fa3ce2a01feb4e6a96993cc5635095ce98a8
scrollshadow <zaldhypramana@gmail.com>
2026-04-20 14:27:41 +0700

    first commit

---
+++ OPP.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+class Mahasiswa{
+public :
+    int nim;
+    string nama;
+    float nilai;
+    
+    void printData(){
+        cout << "NIM : " << nim << endl;
+        cout << "Nama : " << nama << endl;
+        cout << "Nilai : " << nilai << endl;
+    }
+};//batas class
+
+int main(){
+    Mahasiswa mhs;
+    mhs.nim = 2025;
+    mhs.nama = "Gusty";
+    mhs.nilai = 95;
+
+    mhs.printData();
+}
